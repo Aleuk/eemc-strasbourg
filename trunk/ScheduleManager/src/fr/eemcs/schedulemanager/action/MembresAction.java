1b7f81347980ad23204aa3ab7503131f65639150
gchao.fr@gmail.com <gchao.fr@gmail.com@791f60ec-8a0e-bbad-b24f-1fd514b29739>
2012-11-01 21:43:12 +0000

    Ajout creation et lecture bdd en Local

--- trunk/ScheduleManager/src/fr/eemcs/schedulemanager/action/MembresAction.java
+++ trunk/ScheduleManager/src/fr/eemcs/schedulemanager/action/MembresAction.java
@@ -1,5 +1,8 @@
 package fr.eemcs.schedulemanager.action;
 
+import java.util.Date;
+
+import javax.jdo.PersistenceManager;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.ServletActionContext;
@@ -9,8 +12,11 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 import fr.eemcs.schedulemanager.constants.IResponse;
+import fr.eemcs.schedulemanager.database.PMF;
+import fr.eemcs.schedulemanager.entity.ContactVO;
+import fr.eemcs.schedulemanager.helper.FormatHelper;
 
-public class MembresAction {
+public class MembresAction extends LoggerAction{
 	private String url;
 	 
 	public String getUrl() {
@@ -26,7 +32,40 @@ public class MembresAction {
 		UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
 		if(user != null) {
-			return IResponse.SUCCESS;
+			Date dateNaissance = FormatHelper.getDate("02/08/1986", "dd/MM/yyyy");
+			ContactVO contact = new ContactVO("CHAO", "Gabriel", dateNaissance);
+			
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+			try {
+				pm.makePersistent(contact);
+			} finally {
+				pm.close();
+			}
+			return IResponse.LIST;
+		} else {
+			setUrl(userService.createLoginURL(req.getRequestURI()));
+			return IResponse.LOGIN;
+		}
+	}
+	
+	public String list() {
+		HttpServletRequest req = ServletActionContext.getRequest();
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		if(user != null) {
+			return IResponse.LIST;
+		} else {
+			setUrl(userService.createLoginURL(req.getRequestURI()));
+			return IResponse.LOGIN;
+		}
+	}
+	
+	public String add() {
+		HttpServletRequest req = ServletActionContext.getRequest();
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		if(user != null) {
+			return IResponse.LIST;
 		} else {
 			setUrl(userService.createLoginURL(req.getRequestURI()));
 			return IResponse.LOGIN;
