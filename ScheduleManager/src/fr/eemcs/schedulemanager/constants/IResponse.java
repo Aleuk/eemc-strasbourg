b93d7fe3f92eef7b8ac779c525861a4435a351bf
gchao.fr@gmail.com <gchao.fr@gmail.com@791f60ec-8a0e-bbad-b24f-1fd514b29739>
2012-11-08 17:17:20 +0000

    struts + spring + tiles OK

---
+++ ScheduleManager/src/fr/eemcs/schedulemanager/constants/IResponse.java
@@ -0,0 +1,8 @@
+package fr.eemcs.schedulemanager.constants;
+
+public interface IResponse {
+
+	public final static String SUCCESS = "SUCCESS";
+	public final static String LOGIN = "LOGIN";
+	public final static String LIST = "LIST";
+}
