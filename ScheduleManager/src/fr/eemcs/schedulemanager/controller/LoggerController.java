d3b87150122053955ead2db696213f6e989ccdc9
gchao.fr@gmail.com <gchao.fr@gmail.com@791f60ec-8a0e-bbad-b24f-1fd514b29739>
2013-11-30 12:29:20 +0000

    EvenementVO OK

--- ScheduleManager/src/fr/eemcs/schedulemanager/controller/LoggerController.java
+++ ScheduleManager/src/fr/eemcs/schedulemanager/controller/LoggerController.java
@@ -2,14 +2,25 @@ package fr.eemcs.schedulemanager.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
+import javax.jdo.PersistenceManager;
+
+import org.springframework.beans.propertyeditors.CustomCollectionEditor;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+
+import fr.eemcs.schedulemanager.database.PMF;
+import fr.eemcs.schedulemanager.entity.ContactVO;
+import fr.eemcs.schedulemanager.entity.EvenementVO;
 
 public class LoggerController {
 	User user = null;
@@ -29,6 +40,29 @@ public class LoggerController {
 		dateFormat.setLenient(false);
 		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
 	}
+	
+	@InitBinder
+    protected void initBinderMultiSelect(WebDataBinder binder) {
+        binder.registerCustomEditor(List.class, "responsables", new CustomCollectionEditor(List.class)
+          {
+            @Override
+            protected Object convertElement(Object option)
+            {
+            	 PersistenceManager pm = PMF.get().getPersistenceManager();
+                 try {
+
+                     Long id = new Long(String.valueOf(option));
+                     Key k = KeyFactory.createKey("ContactVO", id);
+                     System.out.println(k);
+                     return k;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return option;
+            }
+          });
+    }
 	
 	public String getUrl() {
 		return url;
--- ScheduleManager/war/pages/listeProgrammes.jsp
+++ ScheduleManager/war/pages/listeProgrammes.jsp
@@ -1,11 +1,22 @@
 <%@ taglib uri="http://displaytag.sf.net" prefix="display" %>
 <%@ taglib uri="/WEB-INF/Taglibs.tld" prefix="t" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 		<jsp:include page="/pages/listeParametrage.jsp" /> 
 
+		<table id="dataTable" class="table table-striped table-bordered table-hover">
+			<c:forEach items="${listeProgrammes}" var="mois">
+			    <tr>
+			    	<td>${mois.key}</td>
+			    	<td>${mois.value}</td>
+			    	<td>actions</td>
+			    </tr>
+			</c:forEach>
+		</table>
+		
 		<display:table id="dataTable" name="listeProgrammes"
 			pagesize="15" requestURI="/controller/parametrage/programme" class="table table-striped table-bordered table-hover" decorator="fr.eemcs.schedulemanager.decorator.ProgrammeDecorator">
-			<display:column property="date" keyTitle="table.title.mois" />
+			<display:column property="mois" keyTitle="table.title.mois" />
 			<display:column property="actions" keyTitle="table.title.actions" />
 		</display:table>
 		
