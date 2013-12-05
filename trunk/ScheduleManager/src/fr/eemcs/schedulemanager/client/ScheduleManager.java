025e35832fefc5a6f37c8909e06c157d9ce2bf17
gchao.fr@gmail.com <gchao.fr@gmail.com@791f60ec-8a0e-bbad-b24f-1fd514b29739>
2013-12-05 16:18:05 +0000

    Planning OK

--- trunk/ScheduleManager/src/fr/eemcs/schedulemanager/client/ScheduleManager.java
+++ trunk/ScheduleManager/src/fr/eemcs/schedulemanager/client/ScheduleManager.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -13,6 +16,8 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SingleSelectionModel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -28,24 +33,6 @@ public class ScheduleManager implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		//final DialogBox dialogBox = new DialogBox();
-		//dialogBox.setText("Remote Procedure Call");
-		//dialogBox.setAnimationEnabled(true);
-		//final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		//closeButton.getElement().setId("closeButton");
-		//final Label textToServerLabel = new Label();
-		//final HTML serverResponseLabel = new HTML();
-		//VerticalPanel dialogVPanel = new VerticalPanel();
-		//dialogVPanel.addStyleName("dialogVPanel");
-		//dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		//dialogVPanel.add(textToServerLabel);
-		//dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		//dialogVPanel.add(serverResponseLabel);
-		//dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		//dialogVPanel.add(closeButton);
-		//dialogBox.setWidget(dialogVPanel);
-		
 		// Create a handler for the sendButton and nameField
 		serviceProxy = GWT.create(IAjaxService.class);
 		serviceProxy.getEventsServer("02", new AsyncCallback<List<EvenementInfo>> () {
@@ -56,39 +43,72 @@ public class ScheduleManager implements EntryPoint {
  
 			public void onSuccess(List<EvenementInfo> result) {
 				CellTable<EvenementInfo> table = new CellTable<EvenementInfo>();
-				TextColumn<EvenementInfo> dateColumn = new TextColumn<EvenementInfo>() {
+				TextColumn<EvenementInfo> eventColumn = new TextColumn<EvenementInfo>() {
 					
 					@Override
 					public String getValue(EvenementInfo object) {
-						return object.getDate();
-					}
-				};
-				TextColumn<EvenementInfo> lieuColumn = new TextColumn<EvenementInfo>() {
-					
-					@Override
-					public String getValue(EvenementInfo object) {
+						String lieu = "";
 						if(object.getLieu() != null) {
-							return object.getLieu().getNom();
+							lieu =  object.getLieu().getNom();
 						} else {
 							return "-";
 						}
+						return object.getDate() + " :: " + lieu;
 					}
 				};
 				
-				table.addColumn(dateColumn);
-				table.addColumn(lieuColumn);
+				table.addColumn(eventColumn);
 				if(result != null) {
 					table.setRowCount(result.size(), true);
 				}
 				table.setRowData(result);
+				table.setStyleName("table table-bordered");
 				
-				//dialogBox.setText("Remote Procedure Call");
-				//serverResponseLabel
-				//		.removeStyleName("serverResponseLabelError");
-				//serverResponseLabel.setHTML("kikoulol : " + result.get(0).getDate());
-				//dialogBox.center();
-				//closeButton.setFocus(true);
-
+				final SingleSelectionModel<EvenementInfo> ssm = new SingleSelectionModel<EvenementInfo>();
+				table.setSelectionModel(ssm);
+				ssm.addSelectionChangeHandler(new com.google.gwt.view.client.SelectionChangeEvent.Handler() {
+					
+					@Override
+					public void onSelectionChange(SelectionChangeEvent event) {
+						// TODO Auto-generated method stub
+						final EvenementInfo e = ssm.getSelectedObject();
+						LieuInfo l = e.getLieu();
+						String lNom = "";
+						String lAdresse = "";
+						String lVille = "";
+						if(l != null) {
+							lNom = l.getNom();
+							lAdresse = l.getAdresse();
+							lVille = l.getCodePostal() + " " + l.getVille();
+						}
+						
+						final DialogBox dialogBox = new DialogBox();
+						dialogBox.setText(e.getDate());
+						dialogBox.setAnimationEnabled(true);
+						final Button closeButton = new Button("Fermer");
+						// We can set the id of a widget by accessing its Element
+						closeButton.getElement().setId("closeButton");
+						VerticalPanel dialogVPanel = new VerticalPanel();
+						dialogVPanel.addStyleName("modal-dialog");
+						dialogVPanel.add(new HTML("<b>" + e.getDivers() + "</b>"));
+						dialogVPanel.add(new HTML("<br />"));
+						dialogVPanel.add(new HTML(lNom));
+						dialogVPanel.add(new HTML(lAdresse));
+						dialogVPanel.add(new HTML(lVille));
+						dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+						dialogVPanel.add(closeButton);
+						dialogBox.setWidget(dialogVPanel);
+						
+						// Add a handler to close the DialogBox
+						closeButton.addClickHandler(new ClickHandler() {
+							public void onClick(ClickEvent event) {
+								dialogBox.hide();
+							}
+						});
+						dialogBox.center();
+						closeButton.setFocus(true);
+					}
+				});
 				
 				RootPanel.get("planning").add(table);
 				System.out.println("success");
--- trunk/ScheduleManager/war/templates/planning.jsp
+++ trunk/ScheduleManager/war/templates/planning.jsp
@@ -1,5 +1,6 @@
 <%@ taglib uri="http://displaytag.sf.net" prefix="display" %>
 <%@ taglib uri="/WEB-INF/Taglibs.tld" prefix="t" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> 
-		
+	
+	<div id="titlePlanning"><h4><b>Prochaines réunions</b></h4></div>
 	<div id="planning"></div>
