aa3df49461c1a35cd4b9eb839f4901247bd78eab
gchao.fr@gmail.com <gchao.fr@gmail.com@791f60ec-8a0e-bbad-b24f-1fd514b29739>
2013-12-07 23:15:57 +0000

    Détails programme

--- trunk/ScheduleManager/src/fr/eemcs/schedulemanager/decorator/ProgrammeDecorator.java
+++ trunk/ScheduleManager/src/fr/eemcs/schedulemanager/decorator/ProgrammeDecorator.java
@@ -4,18 +4,27 @@ import java.util.Date;
 
 import org.displaytag.decorator.TableDecorator;
 
+import fr.eemcs.schedulemanager.entity.EvenementVO;
 import fr.eemcs.schedulemanager.helper.FormatHelper;
 
 public class ProgrammeDecorator extends TableDecorator {
-	public String getMois() {
-		String mois = (String)getCurrentRowObject(); //Format MM/yyyy
+	/************* DETAILS_PROGRAMME ************/
+	public String getDate() {
+		EvenementVO e = (EvenementVO)getCurrentRowObject();
 		
-		return FormatHelper.formatDate(FormatHelper.getDate(mois, "MM/yyyy"), "MMMM yyyy").toUpperCase();
+		return FormatHelper.formatDate(e.getDate(), "E MM yyyy").toUpperCase();
+	}
+	
+	/************* PROGRAMME_LIST ************/
+	public String getMois() {
+		String mois = (String)getCurrentRowObject();
+		
+		return FormatHelper.formatDate(FormatHelper.getDate(mois, "yyyy/MM"), "MMMM yyyy").toUpperCase();
 	}
 	
 	public String getActions() {
-		String mois = (String)getCurrentRowObject(); //Format MM/yyyy
-		Date d = FormatHelper.getDate(mois, "MM/yyyy");
+		String mois = (String)getCurrentRowObject();
+		Date d = FormatHelper.getDate(mois, "yyyy/MM");
 		
 		String actions = "";
 		actions += "<a href=\"javascript:getProgramme('" + mois + "');\"><img height=\"25px\" src=\"/images/getProgramme.png\" /></a>&nbsp;&nbsp;";
